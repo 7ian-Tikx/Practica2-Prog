ab9696531249999857880ae40cc0812dc4e989a1
ptian <ptiatnian25@gmail.com>
2026-02-25 17:40:07 +0100

    Classe LlistaReserves implementada al 15%

--- src/prog2/model/LlistaReserves.java
+++ src/prog2/model/LlistaReserves.java
@@ -1,1 +1,13 @@
 package prog2.model;
+
+import java.util.ArrayList;
+
+public class LlistaReserves implements InLlistaReserves{
+    private ArrayList<Reserva> llistaReserves;
+
+    //Fem el seu constructor
+    public LlistaReserves(){
+        this.llistaReserves = new Arraylist<>();
+    }
+    //Implementam metode allotjamentDisponible
+}
