82c7c3ef2ead263ec46d0f8831c1f028553e070e
ptian <ptiatnian25@gmail.com>
2026-02-26 13:55:31 +0100

    50%

--- src/prog2/model/Camping.java
+++ src/prog2/model/Camping.java
@@ -1,5 +1,53 @@
 package prog2.model;
 
-public class Camping implements InCamping {
-    
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Camping implements InCamping{
+    private ArrayList<Allotjament> allotjaments;
+    private ArrayList <Client> clients;
+    private String nom;
+
+    //Fem el constructor de Camping
+    public Camping(ArrayList<Allotjament> allotjaments,ArrayList <Client> clients,String nom) {
+
+        Iterator<Allotjament> iterator = allotjaments.iterator();
+        Iterator<Client> iterator1 = clients.iterator();
+
+        this.nom = nom;
+    }
+
+    //Fem els iterators de allotjaments i clients
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    //Fel els setters de allotjaments, clients i nom
+    public void setAllotjaments(ArrayList<Allotjament> allotjaments){
+        this.allotjaments = allotjaments;
+    }
+    public void setClients(ArrayList <Client> clients){
+        this.clients = clients;
+    }
+    public void setNom(String nom){
+        this.nom = nom;
+    }
+
+    // Fem el mètode buscarAllotjament
+    public String buscarAllotjament(ArrayList<Allotjament> allotjaments){
+            for(int i = 0; i < allotjaments.size(); i++){
+                if(allotjaments.equals(nom)){
+                }
+            }
+    }
+
+    //Fem el mètode buscarClient
+    public String buscarClient(ArrayList <Client> clients){
+
+    }
+    public static InAllotjament.Temp getTemporada(LocalDate data){
+
+    }
 }
--- src/prog2/model/LlistaReserves.java
+++ src/prog2/model/LlistaReserves.java
@@ -1,5 +1,6 @@
 package prog2.model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class LlistaReserves implements InLlistaReserves{
@@ -7,7 +8,21 @@ public class LlistaReserves implements InLlistaReserves{
 
     //Fem el seu constructor
     public LlistaReserves(){
-        this.llistaReserves = new Arraylist<>();
+        this.llistaReserves = new ArrayList<>();
+    }
+    //Fem el seu getter i setter
+    public ArrayList getLlistaReserves(){
+        return this.llistaReserves;
+    }
+    public void setLlistaReserves(ArrayList<Reserva> llistaReserves){
+        this.llistaReserves = llistaReserves;
     }
     //Implementam metode allotjamentDisponible
+    public boolean allotjamentDisponible(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida){
+
+    }
+    //Implementam metode isEstadaMinima
+    public boolean isEstadaMinima (Allotjament allotjament,LocalDate dataEntrada,LocalDate dataSortida){
+
+    }
 }
