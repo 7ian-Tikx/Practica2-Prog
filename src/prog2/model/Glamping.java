9e820358e175f8ef24b91d2b888e2d9bba9b0895
Jinjie Chen <jinjiechen25@gmail.com>
2026-03-02 19:38:20 +0100

    Complet

--- src/prog2/model/Glamping.java
+++ src/prog2/model/Glamping.java
@@ -1,1 +1,43 @@
 package prog2.model;
+
+public class Glamping extends Casa{
+    private String material;
+    private boolean casaMascota;
+
+    // El constructor
+    public Glamping(String nom, String id, long estadaMinimaALTA,long estadaMinimaBAIXA,
+                    String mida,int habitacions,int placesPersones,String material,boolean casaMascota){
+        super(nom,id,3,3, mida, habitacions, placesPersones);
+        this.material = material;
+        this.casaMascota = casaMascota;
+    }
+
+    // Els getters
+    public String getMaterial() {
+        return material;
+    }
+
+    public boolean isCasaMascota() {
+        return casaMascota;
+    }
+
+    // Els setters
+    public void setMaterial(String material){
+        this.material = material;
+    }
+
+    public void setCasaMascota(boolean casaMascota){
+        this.casaMascota = casaMascota;
+    }
+
+    // El metodo funcionaCorrectament
+    public boolean correcteFuncionament(){
+        return casaMascota;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ". Glamping{" + "material='" + material + "'" + ", casaMascota=" + casaMascota + "}";
+    }
+}
--- src/prog2/model/MobilHome.java
+++ src/prog2/model/MobilHome.java
@@ -1,1 +1,32 @@
 package prog2.model;
+
+public class MobilHome extends Casa{
+    private boolean terrassaBarbacoa;
+
+    // El constructor
+    public MobilHome(String nom, String id, long estadaMinimaALTA,long estadaMinimaBAIXA,
+                    String mida,int habitacions,int placesPersones,boolean terrassaBarbacoa){
+        super(nom,id,3,3, mida, habitacions, placesPersones);
+        this.terrassaBarbacoa = terrassaBarbacoa;
+    }
+
+    // Els getters
+    public boolean isCasaMascota() {
+        return terrassaBarbacoa;
+    }
+
+    // Els setters
+    public void setCasaMascota(boolean terrassaBarbacoa){
+        this.terrassaBarbacoa = terrassaBarbacoa;
+    }
+
+    // El metodo funcionaCorrectament
+    public boolean correcteFuncionament(){
+        return terrassaBarbacoa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ". MobilHome{" + "material='" + ", terrassaBarbacoa=" + terrassaBarbacoa + "}";
+    }
+}
