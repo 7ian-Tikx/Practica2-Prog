c274339c4b5cf597630551592d7efe80926030ca
ptian <ptiatnian25@gmail.com>
2026-03-02 16:48:23 +0100

    LlistaReserves implementada al 100%

--- src/prog2/model/LlistaReserves.java
+++ src/prog2/model/LlistaReserves.java
@@ -1,7 +1,9 @@
 package prog2.model;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import prog2.vista.ExcepcioReserva;
 
 public class LlistaReserves implements InLlistaReserves{
     private ArrayList<Reserva> llistaReserves;
@@ -15,14 +17,61 @@ public class LlistaReserves implements InLlistaReserves{
         return this.llistaReserves;
     }
     public void setLlistaReserves(ArrayList<Reserva> llistaReserves){
+
         this.llistaReserves = llistaReserves;
     }
     //Implementam metode allotjamentDisponible
     public boolean allotjamentDisponible(Allotjament allotjament, LocalDate dataEntrada, LocalDate dataSortida){
+        for(int i = 0; i < llistaReserves.size();i++){
+            Reserva reserva = llistaReserves.get(i);
 
+            if(reserva.getAllotjament_().getId().equals(allotjament.getId())){
+                if (dataEntrada.isAfter(reserva.getDataSortida()) && dataSortida.isBefore(reserva.getDataEntrada())){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     //Implementam metode isEstadaMinima
     public boolean isEstadaMinima (Allotjament allotjament,LocalDate dataEntrada,LocalDate dataSortida){
+        long diesEstada = ChronoUnit.DAYS.between(dataEntrada,dataSortida);
+
+        InAllotjament.Temp temporadaActual = Camping.getTemporada(dataEntrada);
+
+        if(temporadaActual == InAllotjament.Temp.ALTA){
+            if(diesEstada >= allotjament.getEstadaMinimaAlta()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(diesEstada >= allotjament.getEstadaMinimaBaixa()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        @Override
+        public void afegirReserva(Allotjament allotjament,Client client, LocalDate dataEntrada,LocalDate dataSortida)throws ExcepcioReserva{
+            if(!allotjamentDisponible(allotjament,dataEntrada,dataSortida){
+                throw new ExcepcioReserva("L'allotjament amb identificador " + allotjament.getId() + "no està disponible en la data demanada "
+                        + dataEntrada + " pel client " + client.getNom() +  " amb DNI: " + client.getDNI());
+            }
+            if(!isEstadaMinima(allotjament,dataEntrada,dataSortida)){
+                throw new ExcepcioReserva("Les dates solicitades pel client " + client.getNom() + " amb DNI: " +
+                        client.getDNI() + " no compleixen l'estada minima per l'allotjament amb identificador " +
+                        allotjament.getId() + ".");
+            }
+
+            Reserva reserva = new Reserva(allotjament,client,dataEntrada,dataSortida);
+            llistaReserves.add(reserva);
+        }
+
 
     }
 }
