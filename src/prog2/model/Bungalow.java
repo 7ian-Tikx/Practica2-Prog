c3fb90f886be7430169618f58a07abfec237a9f4
Jinjie Chen <jinjiechen25@gmail.com>
2026-03-02 19:21:14 +0100

    Complet

--- src/prog2/model/Bungalow.java
+++ src/prog2/model/Bungalow.java
@@ -1,1 +1,63 @@
 package prog2.model;
+
+public class Bungalow extends Casa{
+    private int placesParquing;
+    boolean terrassa;
+    boolean tv;
+    private boolean aireFred;
+
+    // El constructor
+    public Bungalow(String nom, String id, long estadaMinimaALTA,long estadaMinimaBAIXA,
+                String mida,int habitacions,int placesPersones,int placesParquing,boolean terrassa,boolean tv, boolean aireFred){
+       super(nom,id,4,7,mida,habitacions,placesPersones);
+       this.placesParquing = placesParquing; // Un bungallow normal nomes te 1 places de parquing
+       this.aireFred = aireFred;
+    }
+
+    // Els getters
+    public int getPlacesParquing(){
+        return this.placesParquing;
+    }
+
+    public boolean isTerrassa(){
+        return this.terrassa;
+    }
+
+    public boolean isTV(){
+        return this.tv;
+    }
+
+    public boolean isAireFred(){
+        return this.aireFred;
+    }
+
+
+    // Els setters
+    public void setPlacesParquingPlaces(int placesParquing){
+        this.placesParquing = placesParquing;
+    }
+
+    public void setTerrassa(boolean terrassa){
+        this.terrassa = terrassa;
+    }
+
+    public void setTV(boolean tv){
+        this.tv = tv;
+    }
+
+    public void setAireFred(boolean aireFred){
+        this.aireFred = aireFred;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ". Bungalow{" + "placesParquing=" + placesParquing + ", terrassa=" + terrassa +
+                ", tv=" + tv + ", aireFred=" + aireFred + "}";
+    }
+
+    // El metodo funcionaCorrectament
+    public boolean correcteFuncionament(){
+        return aireFred;
+    }
+}
--- src/prog2/model/BungalowPremium.java
+++ src/prog2/model/BungalowPremium.java
@@ -1,1 +1,53 @@
 package prog2.model;
+
+public class BungalowPremium extends Bungalow{
+    private boolean serveisExtra;
+    private String codiWifi;
+
+    // El constructor
+    public BungalowPremium(String nom, String id, long estadaMinimaALTA,long estadaMinimaBAIXA,
+                    String mida,int habitacions,int placesPersones,int placesParquing,boolean terrassa,boolean tv,
+                           boolean aireFred,boolean serveisExtra,String codiWifi){
+        super(nom,id,4,7,mida,habitacions,placesPersones,placesParquing,
+                terrassa,tv,aireFred);
+        this.serveisExtra = serveisExtra;
+        this.codiWifi = codiWifi;
+    }
+
+
+    // Els getters dels nous atributs
+    public boolean isServeisExtra(){
+        return serveisExtra;
+    }
+
+    public String getCodiWifi(){
+        return codiWifi;
+    }
+
+    // Els setters dels nous atributs
+    public void setServeisExtra(boolean serveisExtra){
+        this.serveisExtra = serveisExtra;
+    }
+
+    public void setCodiWifi(String codiWifi){
+        this.codiWifi = codiWifi;
+    }
+
+
+    // El metodo funcionaCorrectament
+    public boolean correcteFuncionament(){
+        boolean codiCorrecte = codiWifi.length() >= 8 && codiWifi.length() <= 16;
+        return codiCorrecte && super.isAireFred();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ". BungalowPremium{" +
+                "serveisExtra=" + serveisExtra +
+                ", codiWifi='" + codiWifi + "'" +
+                "}";
+    }
+
+
+}
