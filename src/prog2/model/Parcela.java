e5122d93e97782ce88ac67a937cdb20e1017b618
Jinjie Chen <jinjiechen25@gmail.com>
2026-03-26 13:32:35 +0100

    Acabado

--- src/prog2/model/Parcela.java
+++ src/prog2/model/Parcela.java
@@ -1,39 +1,41 @@
 package prog2.model;
 
-public class Parcela extends Allotjament{
+public class Parcela extends Allotjament {
     private boolean conexioElectrica;  // Si te o no un punt de connexio
     private float mida;
 
     // El constructor
-    public Parcela(String nom, String id,boolean estat, String iluminacio, float mida, boolean conexioElectrica){
-        super(nom,id,4,2,estat,iluminacio);
+    public Parcela(String nom, String id, boolean estat, String iluminacio, float mida, boolean conexioElectrica) {
+        super(nom, id, 4, 2, estat, iluminacio);
         this.conexioElectrica = conexioElectrica;
         this.mida = mida;
     }
 
     // Els getters i setters dels nous atributs
-    public boolean isConnexioElectrica(){
+    public boolean isConnexioElectrica() {
         return this.conexioElectrica;
     }
 
-    public double getMida(){
+    public double getMida() {
         return this.mida;
     }
 
-    public void setConnexioElectrica(boolean conexioElectrica_){
+    public void setConnexioElectrica(boolean conexioElectrica_) {
         this.conexioElectrica = conexioElectrica_;
     }
 
-    public void setMida(float mida_){
+    public void setMida(float mida_) {
         this.mida = mida_;
     }
 
     @Override
-    public boolean correcteFuncionament(){
+    public boolean correcteFuncionament() {
         return conexioElectrica;
     }
 
     @Override
-    public String toString(){
-       return super.toString() + "Parcela{mida=" + this.mida + ", connexioElectrica=" + this.conexioElectrica + "}";
+    public String toString() {
+        return super.toString() + "Parcela{mida=" + this.mida + ", connexioElectrica=" + this.conexioElectrica + "}";
     }
+
+}
