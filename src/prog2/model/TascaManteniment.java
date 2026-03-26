f8af76e36cf17b8a30b5089f14cabb7de2550f38
Jinjie Chen <jinjiechen25@gmail.com>
2026-03-26 13:14:57 +0100

    Falta getIluminacioAllotjament()

---
+++ src/prog2/model/TascaManteniment.java
@@ -0,0 +1,73 @@
+package prog2.model;
+
+public class TascaManteniment implements InTascaManteniment {
+    private int numTasca;
+    private TipusTascaManteniment tipus;
+    private Allotjament allotjament;
+    private String data;
+    private int dies;
+
+    // Els getters
+    @Override
+    public int getNum() {
+        return numTasca;
+    }
+
+    @Override
+    public TascaManteniment.TipusTascaManteniment getTipus() {
+        return this.tipus;
+    }
+
+    @Override
+    public Allotjament getAllotjament() {
+        return allotjament;
+    }
+
+    @Override
+    public String getData() {
+        return data;
+    }
+
+    @Override
+    public int getDies() {
+        return dies;
+    }
+
+    // Els setters
+    public void setNum(int num_) {
+        this.numTasca = num_;
+    }
+
+    @Override
+    public void setTipus(TascaManteniment.TipusTascaManteniment tipus_) {
+        this.tipus = tipus_;
+    }
+
+    @Override
+    public void setAllotjament(Allotjament allotjament) {
+        this.allotjament = allotjament;
+    }
+
+    @Override
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public void setDies(int dies) {
+        this.dies = dies;
+    }
+
+    @Override
+    public String getIluminacioAllotjament() {
+        return "";
+    }
+
+    public static enum TipusTascaManteniment{
+        Reparacio,
+        Neteja,
+        RevisioTecnica,
+        Desinfeccio
+    };
+}
+
